69bdbf1a75235e233ef9623af3158b475435a3c2
dangnhon <dang.nhon12345@yahoo.com>
2021-01-12 16:07:14 -0800

    commit tree bf7d2cc823b7eb9e99ca60611b0e2d61cbc5dcf7

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    javascriptCool()
+})
+
+function javascriptCool() {
+      let message = document.getElementById('text')
+      message.textContent = "This is really cool!"
+}
